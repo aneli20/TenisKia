b5824b3252f6d177790d4df0335f1d639f3fa3c6
52558 <52558@Kiyoshi2003>
2023-03-26 10:58:50 -0600

    nuevocomi

--- proyectokia/src/main/java/org/teniskia/controller/HomeController.java
+++ proyectokia/src/main/java/org/teniskia/controller/HomeController.java
@@ -3,6 +3,7 @@ package org.teniskia.controller;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ import org.teniskia.entity.Perfil;
 import org.teniskia.entity.Usuario;
 import org.teniskia.service.IntServiceCatalogos;
 import org.teniskia.service.IntServiceUsuarios;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -53,6 +56,12 @@ public class HomeController {
 	@GetMapping("/login")
 	public String mostrarLogin() {
 		return "formLogin";
+	}
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+		logoutHandler.logout(request, null, null);
+		return "redirect:/";
 	}
 	
 	
