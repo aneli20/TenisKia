e2e9a6bd96b5fb3f98893c4bc4191ee0e09c5ec3
52558 <52558@Kiyoshi2003>
2023-03-27 20:24:22 -0600

    ki

--- proyectokia/src/main/java/org/teniskia/controller/HomeController.java
+++ proyectokia/src/main/java/org/teniskia/controller/HomeController.java
@@ -1,16 +1,13 @@
 package org.teniskia.controller;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.teniskia.entity.Perfil;
 import org.teniskia.entity.Usuario;
 import org.teniskia.service.IntServiceCatalogos;
