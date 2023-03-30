0f08575f94c6fc44a39ce38798ecb2ae572a984e
52558 <52558@Kiyoshi2003>
2023-03-30 13:09:21 -0600

    V1.8

---
+++ proyectokia/src/main/java/org/teniskia/TeniasKiaApplication.java
@@ -0,0 +1,13 @@
+package org.teniskia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeniasKiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeniasKiaApplication.class, args);
+	}
+
+}
