4de536728a9d0021644cbc94c0f5f6b17169baae
52558 <52558@Kiyoshi2003>
2023-03-30 12:54:22 -0600

    V1.3

---
+++ proyectokia/src/main/java/org/teniskia/entity/Categoria.java
@@ -0,0 +1,45 @@
+/**
+ * Esta clase representa una entidad (un registro) en la tabla de Categorias de la base de datos
+ */
+package org.teniskia.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment MySQL
+	private Integer id;
+	private String marca;
+	private String clasificacion;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getClasificacion() {
+		return clasificacion;
+	}
+	public void setClasificacion(String clasificacion) {
+		this.clasificacion = clasificacion;
+	}
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", marca=" + marca + ", clasificacion=" + clasificacion + "]";
+	}
+}
